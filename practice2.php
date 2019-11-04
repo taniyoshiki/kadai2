9bb4a88861332e0f9c923bff2db5da3d4f84bc51
”taniyoshiki” <awpo46495@gmail.com>
2019-11-04 02:26:57 +0000

    test

--- practice2.php
+++ practice2.php
@@ -32,3 +32,47 @@ for($i = 1; $i < 101; $i++){
     echo "\n";    
   }    
 }
+
+function sum(){
+  $result = 0;
+  for($i = 1; $i <= 10; $i++){
+    $result += $i;
+  }
+  return $result ;
+}
+echo sum();
+    echo "\n"; 
+
+function times2($num){
+    return $num * 2;
+}
+
+function nibai($val){
+  return $val * 2;
+  
+}
+   echo "\n";
+function f($a, $b){
+  return $a + $b;
+}
+
+$arr = array(1, 3, 5, 7, 9);
+
+
+function g($arr) {
+  $result = 1;
+  foreach($arr as $a){
+    $result = $result * $a;
+  }
+}
+
+$arr = array(1, 3, 11, 7, 9);
+function max_array($arr){
+  $max_number = $arr[0];
+  foreach($arr as $a){ 
+    if($max_number<$a){
+      $max_number = $a;
+    }  
+  }
+  return $max_number;
+}
